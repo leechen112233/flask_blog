a80765c27386c1ed1290ef551d20196b62ff838a
chenDev <chen@okos.ca>
2023-02-16 19:51:00 -0700

    add template htmls, css and mock data

--- app.py
+++ app.py
@@ -1,16 +1,32 @@
 from flask import Flask
+from flask import render_template, url_for
 
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'li chen',
+        'title':'blog post1',
+        'content': ' first post content',
+        'date_posted':'Feb 16 2023'
+    },
+{
+        'author':'Fang Yiting',
+        'title':'blog post2',
+        'content': ' wo shi da sha zi',
+        'date_posted':'Feb 17 2023'
+    }
+]
 
 @app.route("/")
+@app.route("/home")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return render_template('home.html', posts=posts)
 
 
 @app.route("/about")
 def about():
-    return "<p>about！！</p>"
+    return render_template('about.html', title='about')
 
 
 if __name__ == '__main__':
