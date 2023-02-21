97d23e8ecc451b0601a4d0faae7948e6cae618d9
chenDev <chen@okos.ca>
2023-02-20 18:44:21 -0700

    add a save_picture to save form picture from the file field

--- flaskblog/routes.py
+++ flaskblog/routes.py
@@ -3,6 +3,8 @@ from flaskblog.forms import RegisterForm, LoginForm, UpdateAccountForm
 from flaskblog.models import User, Post
 from flaskblog import app, db, bcrypt
 from flask_login import login_user, current_user, logout_user, login_required
+import secrets
+import os
 
 posts = [
     {
@@ -64,11 +66,23 @@ def logout():
         logout_user()
         return redirect(url_for("home"))
 
+def save_picture(form_picture):
+    # randomize the name of the picture
+    random_hex = secrets.token_hex(8)
+    f_name, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex+f_ext
+    picture_path = os.path.join(app.root_path, 'static/profile_pics', picture_fn)
+    form_picture.save(picture_path)
+    return picture_fn
+
 @app.route("/account", methods = ['GET', 'POST'])
 @login_required
 def account():
     form = UpdateAccountForm()
     if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
         current_user.username = form.username.data
         current_user.email = form.email.data
         db.session.commit()
