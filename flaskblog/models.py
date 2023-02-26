96f5248aad16c051c36e7ceb3dc0da5154c778b5
chenDev <chen@okos.ca>
2023-02-25 20:30:47 -0700

    add get_reset_token method and verify_reset_token method into User model

--- flaskblog/models.py
+++ flaskblog/models.py
@@ -1,6 +1,8 @@
 from datetime import datetime
-from flaskblog import db, login_manager
+from flaskblog import db, login_manager, app
 from flask_login import UserMixin #this class has 3 attributes: is_authenticated, is_active, is_annoymous and 1 method: get_user_by_id
+from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
+
 @login_manager.user_loader
 def load_user(user_id):
     return User.query.get(int(user_id)) #flask-login knows this is the function to get the user by id
@@ -11,6 +13,19 @@ class User(db.Model, UserMixin):
     image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
     password = db.Column(db.String(60), nullable=False)
     posts = db.relationship('Post', back_populates='author', lazy=True)
+
+    def get_reset_token(self, expires_sec=1800):
+        s = Serializer(app.config['SECRET_KEY'], expires_sec)
+        return s.dumps({'user_id':self.id}).decode('utf-8')
+
+    @staticmethod
+    def verify_reset_token(token):
+        s = Serializer(app.config['SECRET_KEY'])
+        try:
+            user_id = s.loads(token)['user_id']
+        except:
+            None
+        return User.query.get(user_id)
 
     def __repr__(self):
         return f"User('{self.username}','{self.email}','{self.image_file}')"
