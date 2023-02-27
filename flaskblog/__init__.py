083f76ad8445ff520fb87578b1ef545203eed6c2
leechen112233 <71535717+leechen112233@users.noreply.github.com>
2023-02-26 18:35:58 -0700

    Update __init__.py

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -7,8 +7,6 @@ import os
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'c2ecf156c9f1a38634e9da387fba6bab'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db' #this is the relevant path from the current file
 db = SQLAlchemy(app) #an sqlalchemy database instance
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app) #add some functionalities to the backend, and it will handle the login session
